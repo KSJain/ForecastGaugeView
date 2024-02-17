2aebba9b4031aae086a4c5069e2d00c9fa7f43b0
Kartikeya Saxena Jain <kartikeya.saxena.jain@gmail.com>
2024-02-17 16:50:48 -0500

    Initial Commit

---
+++ ForecastGaugeView/ForecastGaugeViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  ForecastGaugeViewApp.swift
+//  ForecastGaugeView
+//
+//  Created by Kartikeya Saxena Jain on 2/17/24.
+//
+
+import SwiftUI
+
+@main
+struct ForecastGaugeViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
